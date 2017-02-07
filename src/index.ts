62022e2de3fce816cf8d2773a391c80488503c49
dominicboettger <dominic.boettger@inspirationlabs.com>
2017-02-07 17:53:58 +0100

    initial connect

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,37 @@
 'use strict';
-import 'reflect-metadata';
-import {Observable, Subject} from '@reactivex/rxjs';
-import {di, singleton, injectable} from '@molecuel/di';
+import {injectable} from '@molecuel/di';
+import {MongoClient, Db} from 'mongodb';
+import {IMlclDatabase} from '@molecuel/database';
+
+@injectable
+export class MlclMongoDb implements IMlclDatabase {
+  private _database: Db;
+  private _connectionurl: string;
+  constructor() {}
+
+  public async connect(): Promise<Db|Error> {
+    try {
+      this._database = await MongoClient.connect(this._connectionurl);
+      return this._database;
+    } catch(e) {
+      return e;
+    }
+  }
+
+  /**
+   * @description Returns the private database connection
+   * @readonly
+   *
+   * @memberOf MlclMongoDb
+   */
+  public get database() {
+    return this._database;
+  }
+
+  public save(document: Object) {
+    console.log(this._database);
+  }
+
+  public update(document: Object) {
+  }
+}
