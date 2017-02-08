efaedd47522cec9b95b6fef7842335192e8ec9bd
DanielWalther <daniel.walther@inspirationlabs.com>
2017-02-08 12:48:38 +0100

    updated unittessts; methods for single and multi-operations; code coverage

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ export class MlclMongoDb implements IMlclDatabase {
   public async connect(): Promise<Db|Error> {
     try {
       this._database = await MongoClient.connect(this._connectionurl);
-      return this._database;
+      return Promise.resolve(this._database);
     } catch(e) {
-      return e;
+      return Promise.reject(e);
     }
   }
 
@@ -28,27 +28,65 @@ export class MlclMongoDb implements IMlclDatabase {
     return this._database;
   }
 
-  public async save(document: Object, collection: string): Promise<any> {
+  public async save(document: Object, collectionName: string): Promise<any> {
     // console.log(this._database);
     let update = JSON.parse(JSON.stringify(document));
     delete update.id;
     try {
-      if (!this.database[collection]) {
-        let collres = await this.database.createCollection(collection);
-        console.log(collres);
-      }
-      let saved = await this.database[collection].update({id: (<any>document).id}, update, {upsert: true});
-      return Promise.resolve(saved);
+      let saved = await (await this.database.collection(collectionName)).updateOne({_id: (<any>document).id}, update, {upsert: true});
+      return Promise.resolve(saved.result ? saved.result : saved);
     }
     catch (e) {
       return Promise.reject(e);
     }
   }
 
-  public update(document: Object) {
+  public async update(query: Object, update: Object, collectionName: string): Promise<any> {
+    try {
+      let saved = await (await this.database.collection(collectionName)).update(query, update);
+      return Promise.resolve((<any>saved).result ? (<any>saved).result : saved);
+    }
+    catch (e) {
+      return Promise.reject(e);
+    }
   }
 
-  public find(query: Object) {
+  public async updateMany(query: Object, update: Object, collectionName: string): Promise<any> {
+    try {
+      let options: any = {};
+      options.multi = true;
+      let saved = await (await this.database.collection(collectionName)).updateMany(query, update, options, null);
+      return Promise.resolve((<any>saved).result ? (<any>saved).result : saved);
+    }
+    catch (e) {
+      return Promise.reject(e);
+    }
+  }
 
+  public async find(query: Object, collectionName: string): Promise<any> {
+    try {
+      let response = await (await this.database.collection(collectionName)).find(query);
+      return Promise.resolve(response.toArray());
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public async findOne(query: Object, collectionName: string): Promise<any> {
+    try {
+      let response = await (await this.database.collection(collectionName)).find(query);
+      return Promise.resolve(response.next());
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public async dropCollection(collectionName: string): Promise<any> {
+    try {
+      let response = await this.database.dropCollection(collectionName);
+      return Promise.resolve(response);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -13,46 +13,148 @@ describe('mongodb', function() {
   });
   let db;
   describe('init', function() {
+    it('should not connect', async function() {
+      let connection;
+      db = di.getInstance('MlclMongoDb', 'herpderp');
+      try {
+        connection = await db.connect();
+      }
+      catch (e) {
+        should.exist(e);
+      }
+      db.should.be.instanceOf(MlclMongoDb);
+      should.not.exist(connection);
+    });
     it('should connect', async function() {
       let connection;
       db = di.getInstance('MlclMongoDb', 'mongodb://localhost/mongodb_test');
       try {
         connection = await db.connect();
-        // console.log({db:db, connection: connection});
       }
       catch (e) {
         should.not.exist(e);
-        // console.log({db: db, error: e});
       }
       db.should.be.instanceOf(MlclMongoDb);
       should.exist(connection);
     });
   }); // category end
   describe('interaction', function() {
+    const VEHICLE_COLLECTION = 'vehicle_test';
+    const ENGINE_COLLECTION = 'engine_test';
     let testCar = {
       id: 1,
       model: 'C4',
       make: 'Aston Maaaartin',
       engine: 1
-    }
+    };
+    let testTruck = {
+      id: 2,
+      model: 'BRMM',
+      make: 'STAB',
+      engine: 4
+    };
+    it('should not save a new document (no collection supplied)', async function() {
+      try {
+        let response = await db.save(testCar);
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
+    });
     it('should save a new document (in its collection)', async function() {
-      // let response = await db.save(testCar, 'cars_test');
-      // console.log(response);
+      try {
+        let response = await db.save(testCar, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
     });
-    it('should find the saved document (in its collection)', function() {
-
+    it('should not find any saved document (no collection supplied)', async function() {
+      try {
+        let response = await db.findOne({_id: testCar.id});
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
+      try {
+        let response = await db.find({_id: testCar.id});
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
     });
-    it('should save another new document (in its collection)', function() {
-
+    it('should find the single saved document (in its collection)', async function() {
+      try {
+        let response = await db.findOne({_id: testCar.id}, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
     });
-    it('should update a document (in its collection)', function() {
-
+    it('should save another new document (in its collection)', async function() {
+      try {
+        let response = await db.save(testTruck, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
     });
-    it('should find all saved documents (in one collection)', function() {
-
+    it('should not update anything (no collection supplied)', async function() {
+      let update = JSON.parse(JSON.stringify(testCar));
+      update.engine = 2;
+      try {
+        let response = await db.update(testCar, update);
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
+      try {
+        let response = await db.updateMany({}, {$set: {engine: 3}});
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
     });
-    it('should update all saved documents (in one collection)', function() {
-
+    it('should update a document (in its collection)', async function() {
+      let update = JSON.parse(JSON.stringify(testCar));
+      update.engine = 2;
+      try {
+        let response = await db.update(testCar, update, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
+    });
+    it('should find all saved documents (in one collection)', async function() {
+      try {
+        let response = await db.find({}, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
+    });
+    it('should update all saved documents (in one collection)', async function() {
+      try {
+        let response = await db.updateMany({}, {$set: {engine: 3}}, VEHICLE_COLLECTION);
+        should.exist(response);
+      } catch (error) {
+        should.not.exist(error);
+      }
+    });
+    it('should receive error when attemting to drop non-existent collecgion', async function() {
+      try {
+        let response = await db.dropCollection(ENGINE_COLLECTION);
+        should.not.exist(response);
+      } catch (error) {
+        should.exist(error);
+      }
+    });
+    after(async function () {
+      try {
+        await db.dropCollection(VEHICLE_COLLECTION);
+      } catch (error) {
+        should.not.exist(error);
+      }
     });
   }); // category end
 }); // test end
